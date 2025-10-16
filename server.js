369a520f9af92ff50c94b980663526e117c7c0ad
Thamingalo <168442579+Thamingalo@users.noreply.github.com>
2025-10-16 19:22:04 +0200

    Add CORS support and update server log message

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import cors from 'cors';
 import paymentRoutes from './routes/paymentRoutes.js';
 
-dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use('/api/payments', paymentRoutes);
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`💳 Ozow Test Backend running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Ozow backend running on port ${PORT}`));
